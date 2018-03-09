132311eb468fc0b715205e0616ea5ce2b37e0d55
chentianming11 <chentianming11@qq.com>
2018-03-09 18:49:25 +0800

    测试枚举

--- src/test/java/com/lianjia/enumtest/TestEnum.java
+++ src/test/java/com/lianjia/enumtest/TestEnum.java
@@ -1,8 +1,16 @@
 package com.lianjia.enumtest;
 
+
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimaps;
 import com.lianjia.entity.User;
-import com.lianjia.util.EnumUtils;
+import org.apache.commons.lang3.EnumUtils;
 import org.junit.Test;
+
+import java.lang.reflect.Field;
+import java.util.*;
+
 
 /**
  * Created by chen on 2018/3/5.
@@ -11,12 +19,58 @@ public class TestEnum {
 
     @Test
     public void test1(){
-        String 交易员 = EnumUtils.name(User.QuestionEnum.class, 2);
-        System.out.println(交易员);
 
-        Object o = EnumUtils.value(User.QuestionEnum.class, 2);
-        System.out.println(o.getClass());
-        User.QuestionEnum questionEnum = (User.QuestionEnum) o;
-        System.out.println(questionEnum.question);
+    }
+
+    @Test
+    public void test2() {
+
+        List<User.QuestionEnum> enumList = EnumUtils.getEnumList(User.QuestionEnum.class);
+
+        ArrayListMultimap<Object, Object> multimap = ArrayListMultimap.create();
+
+        enumList.forEach(questionEnum -> {
+            try {
+                Object id = questionEnum.getClass().getField("id").get(questionEnum);
+                System.out.println(id);
+
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (NoSuchFieldException e) {
+                e.printStackTrace();
+            }
+        });
+
+        System.out.println(JSON.toJSONString(multimap.asMap()));
+    }
+
+    /**
+     * 将Enum转换成MultiMap形式
+     * Enum的name作为key，Enum的自定义属性作为值
+     *
+     * @throws NoSuchFieldException
+     */
+    @Test
+    public void test3() throws NoSuchFieldException {
+        User.QuestionEnum[] enums = User.QuestionEnum.values();
+        Field[] fields = User.QuestionEnum.class.getFields();
+        List<Map<String, Object>> list = new ArrayList<>();
+        for (User.QuestionEnum anEnum : enums) {
+            Map<String, Object> map = new HashMap<>();
+            for (int i = enums.length; i < fields.length; i++) {
+                try {
+                    map.put("name", anEnum.name());
+                    map.put(fields[i].getName(), fields[i].get(anEnum));
+
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            list.add(map);
+        }
+
+        System.out.println(list);
+
     }
 }
