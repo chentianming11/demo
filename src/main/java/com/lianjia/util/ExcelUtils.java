ac01069e57f6b1890068993cc3f754c44d373409
chentianming11 <chentianming11@qq.com>
2018-05-23 22:03:46 +0800

    分页查询,生成SXSSFWorkbook

---
+++ src/main/java/com/lianjia/util/ExcelUtils.java
@@ -0,0 +1,67 @@
+package com.lianjia.util;
+
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.springframework.util.CollectionUtils;
+
+import java.util.*;
+import java.util.function.Function;
+
+/**
+ * Created by chen on 2018/5/23.
+ */
+public class ExcelUtils {
+
+    /**
+     * 分页查询,生成SXSSFWorkbook
+     * @param pageSize
+     * @param sheetName
+     * @param head
+     * @param columnNameList
+     * @param function
+     * @return
+     */
+    public static SXSSFWorkbook createSXSSFWorkbookByPageQuery(Integer pageSize, String sheetName, String[] head, String[] columnNameList, Function<Integer ,List<Map<String, Object>>> function){
+        SXSSFWorkbook workbook = new SXSSFWorkbook();
+        Sheet detailSheet = workbook.createSheet(sheetName + DateFormatUtils.format(new Date(), "yyyyMMdd") );
+        ExcelUtils.sheetAppendRows(detailSheet, head);
+        Integer pageNum = 1;
+        List<Map<String, Object>> result;
+        do {
+            result = function.apply(pageNum);
+            pageNum++;
+            result.forEach(item -> {
+                List<String> list = new ArrayList<>();
+                for (int i = 0; i < columnNameList.length; i++) {
+                    detailSheet.setColumnWidth(i, 10 * 512);
+                    Object o = item.get(columnNameList[i]);
+                    if (Objects.isNull(o)){
+                        o = "";
+                    }
+                    if (o instanceof Date){
+                        o = DateFormatUtils.format((Date) o, "yyyy/MM/dd HH:mm:ss");
+                    }
+                    list.add(String.valueOf(o));
+                }
+                ExcelUtils.sheetAppendRows(detailSheet, list.toArray(new String[list.size()]));
+            });
+
+        }while (!CollectionUtils.isEmpty(result) && result.size() == pageSize);
+        return workbook;
+    }
+
+    /**
+     * 向excel的sheet中追加一行记录
+     * @param sheet
+     * @param data
+     */
+    public static void sheetAppendRows(Sheet sheet, String[] data) {
+        Row row = sheet.createRow(sheet.getPhysicalNumberOfRows());
+        for (int i = 0; i < data.length; i++) {
+            row.createCell(i).setCellValue(data[i] == null ? "" : data[i]);
+        }
+    }
+
+}
