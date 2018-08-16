4421305d0c7b2df6e68fad9a35708cf63cb14f4b
陈添明 <chentianming11@qq.com>
2018-08-16 22:28:12 +0800

    下划线转驼峰

--- src/main/java/com/study/demo/config/InterceptorConfig.java
+++ src/main/java/com/study/demo/config/InterceptorConfig.java
@@ -1,7 +1,6 @@
 package com.study.demo.config;
 
 import com.study.demo.interceptor.LoginInterceptor;
-import com.study.demo.interceptor.MyInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -15,19 +14,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class InterceptorConfig extends WebMvcConfigurerAdapter {
 
     @Autowired
-    MyInterceptor myInterceptor;
-
-    @Autowired
     LoginInterceptor loginInterceptor;
-
-
-
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-
-        registry.addInterceptor(myInterceptor)
-                .addPathPatterns("/**");
 
         // account 登录拦截器
         registry.addInterceptor(loginInterceptor)
